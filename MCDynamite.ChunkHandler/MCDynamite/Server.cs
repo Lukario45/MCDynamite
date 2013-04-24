dd4e810af480a708b5a2b3d3047e03825bf1564b
MonkeyPwns <monkeypwns1@gmail.com>
2013-04-23 17:06:17 -0700

    Added stopServer

--- MCDynamite.ChunkHandler/MCDynamite/Server.cs
+++ MCDynamite.ChunkHandler/MCDynamite/Server.cs
@@ -8,9 +8,12 @@ namespace MCDynamite
     public class Server
     {
         public string motd = "[MCDynamite] Default";
-        public int port = 25565;
-        public bool online = true;
         public string version = "1.0.0.0";
+
+        public int port = 25565;
+
+        public bool online = true;
+        public bool isRunning = false;
 
         public static Server server;
 
@@ -22,6 +25,11 @@ namespace MCDynamite
         public void startServer()
         {
             Console.Title = motd + " | MCDynamite v" + getServer().version;
+        }
+
+        public void stopServer()
+        {
+            Environment.Exit(0);
         }
 
         public void whileRunning()
