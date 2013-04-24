29b37c73881f95413f3f76790e8422e935c9a719
MonkeyPwns <monkeypwns1@gmail.com>
2013-04-24 16:28:58 -0700

    Added Player

---
+++ MCDynamite/Client/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCDynamite.Client
+{
+    public abstract class Player
+    {
+        public abstract string name { get; }
+        public abstract string ip { get; }
+    }
+}
