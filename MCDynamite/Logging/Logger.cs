253ffba95cdae6cfc5b8b9f4687dba7bc635a592
MonkeyPwns <monkeypwns1@gmail.com>
2013-04-24 16:28:21 -0700

    Updated Logger

---
+++ MCDynamite/Logging/Logger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCDynamite.Logging
+{
+    public class Logger
+    {
+        public delegate void LogEventHandler();
+        public static event LogEventHandler onLog;
+        public static event LogEventHandler onLoggerDelete;
+
+        public string name;
+
+        public Logger(string name)
+        {
+            this.name = name;
+            Server.getServer().loggers.Add(this);
+        }
+
+        public void deleteLogger(string name)
+        {
+            foreach (Logger l in Server.getServer().loggers)
+            {
+                if (l.name == name)
+                {
+                    Server.getServer().loggers.Remove(l);
+
+                    if (onLoggerDelete != null)
+                    {
+                        onLoggerDelete();
+                    }
+                }
+            }
+        }
+
+        public static void Log(string log)
+        {
+            if (onLog != null) 
+            {
+                onLog();
+            } 
+            else 
+            {
+                Console.WriteLine("[" + DateTime.Now.ToString("hh:mm:ss") + "] " + log);
+            }
+        }
+    }
+}
