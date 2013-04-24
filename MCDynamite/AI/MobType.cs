5b2610f59bc39051ca2f5d061a5732b5eab2dfa6
MonkeyPwns <monkeypwns1@gmail.com>
2013-04-24 16:28:28 -0700

    Added MobType

---
+++ MCDynamite/AI/MobType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCDynamite.AI
+{
+    public enum MobType
+    {
+        Passive,
+        Neutral,
+        Hostile,
+        Boss
+    }
+}
