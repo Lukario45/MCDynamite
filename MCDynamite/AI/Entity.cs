6d31a7f67aefc6f7253e72541c9eced54b117ade
MonkeyPwns <monkeypwns1@gmail.com>
2013-04-24 16:28:34 -0700

    Added Mobs

---
+++ MCDynamite/AI/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCDynamite.AI
+{
+    public abstract class Entity
+    {
+        public delegate void MobEventHandler();
+
+        public abstract void onKill();
+        public abstract void onSpawn();
+        public abstract MobType type { get; }
+        public abstract int health { get; }
+    }
+}
