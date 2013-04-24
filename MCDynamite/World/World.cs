42946515601ff66acb005a9ee5eb6902dc8d8b56
MonkeyPwns <monkeypwns1@gmail.com>
2013-04-24 16:28:11 -0700

    Added World

---
+++ MCDynamite/World/World.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MCDynamite.AI;
+
+namespace MCDynamite
+{
+    public abstract class World
+    {
+        public abstract string name { get; }
+        public abstract Block[] blocks { get; }
+        public List<Entity> entities = new List<Entity>();
+
+        public void Compress()
+        {
+        }
+
+        public void Decompress()
+        {
+        }
+    }
+}
