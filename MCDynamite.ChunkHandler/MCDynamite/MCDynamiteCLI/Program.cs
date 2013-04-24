baba2d74e276f5ac7147d141caca1ae98ce39753
MonkeyPwns <monkeypwns1@gmail.com>
2013-04-23 17:06:31 -0700

    Events for console exit

--- MCDynamite.ChunkHandler/MCDynamite/MCDynamiteCLI/Program.cs
+++ MCDynamite.ChunkHandler/MCDynamite/MCDynamiteCLI/Program.cs
@@ -8,12 +8,20 @@ namespace MCDynamiteCLI
 {
     class Program
     {
+        static void OnProcessExit(object sender, EventArgs e)
+        {
+            Server.getServer().stopServer();
+        }
+
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
+
             Server.getServer().startServer();
 
             while (true)
             {
+                Server.getServer().whileRunning();
             }
         }
     }
