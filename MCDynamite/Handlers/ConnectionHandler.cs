109dc0b0295a102c93771f206d840df955257c60
MonkeyPwns <monkeypwns1@gmail.com>
2013-05-04 17:39:43 -0700

    Added Player and Connect handlers

---
+++ MCDynamite/Handlers/ConnectionHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MCDynamite.Handlers
+{
+    public class ConnectionHandler
+    {
+        private static TcpListener _listener;
+
+        public ConnectionHandler()
+        {
+            StartListening();
+        }
+
+        private static void StartListening()
+        {
+            while (Server.getServer().isRunning)
+            {
+                try
+                {
+                    _listener = new TcpListener(IPAddress.Any, Server.getServer().port);
+                    _listener.Start();
+                    _listener.BeginAcceptTcpClient(AcceptCallback, _listener);
+                    break;
+                }
+                catch (SocketException e)
+                {
+                    Logging.Logger.Log("Error Code: " + e.ErrorCode);
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Logging.Logger.Log("An error occured!");
+                }
+            }
+        }
+
+        private static void AcceptCallback(IAsyncResult ar)
+        {
+            var listener2 = (TcpListener)ar.AsyncState;
+            try
+            {
+                TcpClient clientSocket = listener2.EndAcceptTcpClient(ar);
+                PlayerHandler.newPlayer(new Player(clientSocket));
+            }
+            catch (Exception e)
+            {
+                Logging.Logger.Log("A Connection error occured!");
+            }
+
+            if (Server.getServer().isRunning)
+            {
+                _listener.BeginAcceptTcpClient(AcceptCallback, _listener);
+            }
+        }
+    }
+}
---
+++ MCDynamite/Handlers/PlayerHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCDynamite.Handlers
+{
+    public class PlayerHandler
+    {
+        public static List<Player> players = new List<Player>();
+
+        public static void newPlayer(Player p)
+        {
+            players.Add(p);
+        }
+
+        public static void delPlayer(Player p)
+        {
+            players.Remove(p);
+        }
+    }
+}
