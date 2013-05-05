5ed91bdf6b6dfda04e539b739ab743bfae3b6040
MonkeyPwns <monkeypwns1@gmail.com>
2013-05-05 14:24:02 -0700

    Added Op List

---
+++ MCDynamite/Util/OpList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCDynamite.Util
+{
+    public class OpList<Player> : List<Player>
+    {
+        public void Add(Player p)
+        {
+            base.Add(p);
+        }
+
+        public void Remove(Player p)
+        {
+            base.Remove(p);
+        }
+    }
+}
