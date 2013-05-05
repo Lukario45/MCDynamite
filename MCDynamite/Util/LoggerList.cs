e322b4256ef69087c04ca246c6998a4646e0a34e
MonkeyPwns <monkeypwns1@gmail.com>
2013-05-05 14:23:57 -0700

    Added Logger List

---
+++ MCDynamite/Util/LoggerList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCDynamite.Util
+{
+    public class LoggerList<Logger> : List<Logger>
+    {
+        public void Add(Logger l)
+        {
+            base.Add(l);
+            
+        }
+
+        public void Remove(Logger l)
+        {
+            base.Remove(l);
+        }
+    }
+}
