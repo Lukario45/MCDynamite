37f471c521c010187d4164a475f0d9a1f904ba5d
MonkeyPwns <monkeypwns1@gmail.com>
2013-05-07 16:11:51 -0700

    Added Blocks

--- MCDynamite/World/World.cs
+++ MCDynamite/World/World.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MCDynamite.Blocks;
 
 namespace MCDynamite
 {
     public abstract class World
     {
         public abstract string name { get; }
-        public abstract Block[] blocks { get; }
+        public abstract BlockData[] blocks { get; }
         public List<Entity> entities = new List<Entity>();
 
         public void Compress()
